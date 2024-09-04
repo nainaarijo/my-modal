884e46fe57e85059793b25a6ed5db9568668b40e
Naina Arijo <167599326+nainaarijo@users.noreply.github.com>
2024-09-04 10:23:34 +0500

    Add files via upload
    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let Modal = document.getElementById('myModal');
+
+function openModal(){
+    Modal.style.display = "block";
+}
+  
+
+function closeModal(){
+    Modal.style.display = "none";
+}
+
